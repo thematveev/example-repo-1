4169135d6eb05682a632afac442d68a651ad9e71
Matveev Maksim <thematveev21@gmail.com>
2025-10-15 17:42:36 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+from threading import Thread
+
+
+def upload_file(name):
+    for i in range(1, 11):
+        print(f"Uploading file {name} Part: {i}")
+        time.sleep(0.5)
+
+
+
+
+print("End of main")
